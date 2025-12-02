a6e718501fdcad8ed4a639692cf2080bad795bae
Zayed Ahmed <zayed967@yahoo.com>
2025-12-02 11:28:11 -0500

    half of resume code

--- train_ddp.py
+++ train_ddp.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.distributed as dist
-import torch.multiprocessing as mp
+from torch.multiprocessing.spawn import spawn
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 from model import SimpleCNN
@@ -10,6 +10,8 @@ from data import get_dataloader
 from utils import setup, cleanup
 
 import argparse
+import os
+from pathlib import Path
 
 
 def ddp_train(rank, world_size, epochs, batch_size):
@@ -21,6 +23,11 @@ def ddp_train(rank, world_size, epochs, batch_size):
     epochs:         number of training loops
     batch_size:     size of chunks to run per GPU
     """
+
+    ##################
+    ##    Set up    ##
+    ##################
+
     setup(rank=rank, world_size=world_size)     # connect the process to the group and set the correct GPU.
     print(f"Rank {rank} initialized")
     model = SimpleCNN().to(rank)                # core model
@@ -37,8 +44,92 @@ def ddp_train(rank, world_size, epochs, batch_size):
     model.train()                                   # turns on batchnorm + dropout
     print(f"Rank {rank}: Loss and Optimizer ready")
 
-    # Training loop 
-    for epoch in range(epochs):
+    ##################
+    ## Pre-Training ##
+    ##################
+
+    flag = torch.tensor([0], dtype=torch.int64)
+    latest_file = None
+
+    if rank == 0:   # only for first process to prevent overwritting ... etc
+        dir = Path("./checkpoints")
+        if dir.exists():
+            files = [f for f in dir.iterdir() if f.is_file()]
+            if files:
+                latest_file = max(files, key=lambda f: f.stat().st_ctime)
+                flag[0] = 1 
+        else:
+            os.mkdir("./checkpoints") # Make sure folder exist
+    
+    dist.broadcast(flag, src=0)                 # Tell everyone about checkpoint
+    dist.barrier()                          # wait until folder is created
+    epoch_tensor = torch.tensor([0], 
+                                dtype=torch.int64)
+    num_key_tensor = torch.tensor([0], 
+                                  dtype=torch.int64)   
+    num_keys = 0
+
+    if flag[0]:
+        if rank ==0:
+            # Load checkpoint
+            ckpt = torch.load(str(latest_file), map_location="cpu")
+            model_state = ckpt["model"]
+            optimizer_state = ckpt["optimizer"]
+            start_epoch = ckpt["epoch"] + 1
+            epoch_tensor[0] = start_epoch
+
+            # keys
+            num_keys = len(model_state.keys())
+            num_key_tensor[0] = num_keys        # All ranks know num of keys
+
+            # sort keys to insure consistency among all ranks(Processes)
+            keys = sorted(model_state.keys())
+            for key in keys:
+                shape = model_state[key].shape
+            
+    # Broadcast epoch and number of keys
+    dist.broadcast(epoch_tensor, src=0)
+    dist.broadcast(num_key_tensor, src=0)
+    
+    all_keys = []
+    for i in range(num_keys):
+
+        # 1. Each rank creates key_len_tensor = [0]
+        key_len_tensor = torch.tensor([0], dtype=torch.int64)
+
+        # 2. Rank 0 fills the key_len_tensor
+        if rank == 0:
+            key = keys[i]
+            key_bytes = key.encode("utf-8")
+            key_len_tensor[0] = len(key_bytes)
+        
+        # 3. Broadcast key length
+        dist.broadcast(key_len_tensor)
+
+        # 4. All ranks allocate recv_buffer = uint8[length]
+        recv_buffer = torch.zeros(key_len_tensor.item(), dtype=torch.uint8)
+
+        # 5. Rank 0 copies bytes into recv_buffer
+        if rank == 0:
+            recv_buffer[:] = torch.tensor(list(key_bytes), dtype=torch.uint8)
+
+        # 6. Broadcast recv_buffer
+        dist.broadcast(recv_buffer, src=0)
+
+        # 7. All ranks decode to string
+        key_str = bytes(recv_buffer.tolist()).decode("utf-8")
+
+        # 8. Append to all_keys
+        all_keys.append(key_str)
+
+    start_epoch = int(epoch_tensor.item())
+
+
+    ##################
+    ##   Training   ##
+    ################## 
+
+    for epoch in range(start_epoch, epochs):
         sampler.set_epoch(epoch)    # for shuffling data for each epoch
         print(f"Rank {rank}: Starting epoch {epoch}")
         for batch_idx, (images, labels) in enumerate(dataloader):
@@ -53,6 +144,14 @@ def ddp_train(rank, world_size, epochs, batch_size):
             if rank == 0:
                 print(f"Epoch: {epoch} Loss: {loss}")
 
+        if rank == 0:
+            torch.save({
+                "epoch": epoch,
+                "model": model.module.state_dict(),
+                "optimizer": opt.state_dict()
+            },f"./checkpoints/epoch_{epoch}.pt")
+
+
     cleanup()
 
 
@@ -63,7 +162,7 @@ def main():
     args = parser.parse_args()
 
     world_size = torch.cuda.device_count()
-    mp.spawn(ddp_train, 
+    spawn(ddp_train, 
              args=(world_size, args.epochs, args.batch_size), 
              nprocs=world_size)
 
